b7ad198a34a7c871dc7fcf1070d10b14090bf62b
Monsieur <christopher.chhim123@gmail.com>
2024-07-18 21:42:55 -0700

    Updated index.js file to prompt user questions. Also added writeToFile and initialize functions

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const questions = [{
+    type: 'input',
+    message: 'What shape would you like to use for your logo?',
+    name: 'shape',
+    choices: ["triangle", "square", "circle"]
+  },
+  {
+    type: 'input',
+    message: 'What color would you like your shape to be?',
+    name: 'color',
+  },
+  {
+    type: 'input',
+    message: 'What would you like your logo to say?',
+    name: 'logo',
+  },
+];
+
 
 function Shapes(edges){
     this.edges = edges;
 }
+
+
+
+
+
+function writeToFile(data) {
+    fs.writeFile("Sample.svg", data, (err) => {
+        err ? console.log(err) : console.log('Successfully created svg file!')
+    });
+  }
+
+
+
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((data) => {
+      console.log("Generating svg logo...")
+      const svg = generateMarkdown(data)
+      writeToFile(svg);
+    })
+}
+
+init();
